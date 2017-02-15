43bd0d2b95e852c532ed62d3e505f16f77600fdc
Kamil Dudka <kdudka@redhat.com>
2017-02-15 18:52:48 +0100

    README: replace references to fedorahosted.org

--- make-srpm.sh
+++ make-srpm.sh
@@ -79,8 +79,8 @@ Summary:    A compiler wrapper that runs cppcheck in background
 
 Group:      Development/Tools
 License:    GPLv3+
-URL:        https://git.fedorahosted.org/cgit/cscppc.git
-Source0:    https://git.fedorahosted.org/cgit/cscppc.git/snapshot/$SRC
+URL:        https://github.com/kdudka/%{name}
+Source0:    https://github.com/kdudka/%{name}/releases/download/%{name}-%{version}/%{name}-%{version}.tar.xz
 
 BuildRequires: asciidoc
 BuildRequires: cmake
